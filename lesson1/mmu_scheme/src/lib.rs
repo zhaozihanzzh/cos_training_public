6d49478850f5f1ec6e97f4003a8c0c032320edd6
zhaozihanzzh <zhaozihanrxh@163.com>
2023-05-17 23:55:46 +0800

    Finish mmu_alterable and mmu_scheme

--- lesson1/mmu_scheme/src/lib.rs
+++ lesson1/mmu_scheme/src/lib.rs
@@ -1,15 +1,74 @@
 #![no_std]
+#![feature(asm_const)]
+
+use riscv::register::satp;
 
 pub const KERNEL_BASE: usize = 0xffff_ffff_c000_0000;
+const PHYS_VIRT_OFFSET: usize = 0xffff_ffc0_0000_0000;
 
+#[cfg(feature = "sv39")]
+#[link_section = ".data.boot_page_table"]
+static mut BOOT_PT_SV39: [u64; 512] = [0; 512];
+#[cfg(feature = "sv48")]
+#[link_section = ".data.boot_page_table"]
+static mut BOOT_PTL3_SV48: [u64; 512] = [0; 512];
+#[cfg(feature = "sv48")]
+#[link_section = ".data.boot_page_table"]
+static mut BOOT_PTL4_SV48: [u64; 512] = [0; 512];
+#[cfg(feature = "sv39")]
 pub unsafe fn pre_mmu() {
-    todo!("dummy");
-}
+    // 0x8000_0000..0xc000_0000, VRWX_GAD, 1G block
+    BOOT_PT_SV39[2] = (0x80000 << 10) | 0xef; // 似乎是跳板？
+    // 只有 R W X 全为 0 时才不是叶子节点，此时已经是叶子了。
+    // 0xffff_ffc0_8000_0000..0xffff_ffc0_c000_0000, VRWX_GAD, 1G block // 左闭右开区间
+    BOOT_PT_SV39[0x102] = (0x80000 << 10) | 0xef; // 链接时把内核代码段放这里了
 
+    // 0xffff_ffff_c000_0000..highest, VRWX_GAD, 1G block
+    BOOT_PT_SV39[0x1ff] = (0x80000 << 10) | 0xef; // e3 is ok // 从 KERNEL_BASE 开始
+    // ffff_ffff_c000_0000 -> 0000_0000_8000_0000 内核在物理地址 0x80200000
+}
+#[cfg(feature = "sv48")]
+pub unsafe fn pre_mmu() {
+    BOOT_PTL3_SV48[2] = (0x80000 << 10) | 0xef;//((BOOT_PTL2_SV48.as_ptr() as u64) >> 12 << 10) | 0x01;
+    BOOT_PTL3_SV48[0x102] = (0x80000 << 10) | 0xef;//((BOOT_PTL2_SV48.as_ptr() as u64) >> 12 << 10) | 0x01;
+    BOOT_PTL3_SV48[0x1ff] = (0x80000 << 10) | 0xef;// ((BOOT_PTL2_SV48.as_ptr() as u64) >> 12 << 10) | 0x01;
+
+    BOOT_PTL4_SV48[0] = ((BOOT_PTL3_SV48.as_ptr() as u64) >> 12 << 10) | 0x01; // 似乎是跳板？
+    BOOT_PTL4_SV48[0x1ff] = ((BOOT_PTL3_SV48.as_ptr() as u64) >> 12 << 10) | 0x01; // e3 is ok // 从 KERNEL_BASE 开始
+    // ffff_ffff_c000_0000 -> 0000_0000_8000_0000 内核在物理地址 0x80200000
+}
+#[cfg(feature = "sv39")]
 pub unsafe fn enable_mmu() {
-    todo!("dummy");
-}
+    if cfg!(feature = "sv39") {
+        let page_table_root = BOOT_PT_SV39.as_ptr() as usize;
+        satp::set(satp::Mode::Sv39, 0, page_table_root >> 12);
+        riscv::asm::sfence_vma_all();
+        
+    } else if cfg!(feature = "sv48") {
 
+    } else if cfg!(feature = "disable") {
+        satp::set(satp::Mode::Bare, 0, 0);
+        riscv::asm::sfence_vma_all();
+    }
+}
+#[cfg(feature = "sv48")]
+pub unsafe fn enable_mmu() {
+    if cfg!(feature = "sv48") {
+        let page_table_root = BOOT_PTL4_SV48.as_ptr() as usize;
+        satp::set(satp::Mode::Sv48, 0, page_table_root >> 12);
+        riscv::asm::sfence_vma_all();
+        
+    }
+}
 pub unsafe fn post_mmu() {
-    todo!("dummy");
+    if !cfg!(feature = "disable") {
+        // 开启页表后跳转至高地址来访问内核，虽然低地址仍然也是有效的
+        core::arch::asm!("
+            li      t0, {phys_virt_offset}  // fix up virtual high address
+            add     sp, sp, t0
+            add     ra, ra, t0
+            ret     ",
+            phys_virt_offset = const PHYS_VIRT_OFFSET,
+        );
+    }
 }
